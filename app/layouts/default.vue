d8f1475fb0a6d33aaa113fb78268f928891a2155
kopki <kopki684@gmail.com>
2026-01-05 01:31:27 +0800

    feat: enhance transition effects for content loading in layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,15 +2,22 @@
 import AppNavbar from '~/components/AppNavbar.vue';
 import AppFooter from '~/components/AppFooter.vue';
 import BackToTopButton from '~/components/BackToTopButton.vue';
+
+const route = useRoute();
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen">
     <AppNavbar />
 
-    <div class="flex-1 max-w-2xl w-full mx-auto mt-26 px-4">
-      <slot />
-    </div>
+    <Transition name="fade" mode="out-in" appear>
+      <div
+        :key="route.fullPath"
+        class="flex-1 max-w-2xl w-full mx-auto mt-26 px-4"
+    >
+        <slot />
+      </div>
+    </Transition>
 
     <AppFooter />
     <BackToTopButton />
